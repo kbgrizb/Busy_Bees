0406b9d2ae513e5eb65722277fb21564efcead7e
kbgrizb <grisbykb@hendrix.edu>
2024-10-10 19:47:15 -0500

    Days can be selected

--- flutter_application_1/lib/calendar_page.dart
+++ flutter_application_1/lib/calendar_page.dart
@@ -2,12 +2,20 @@
 
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-
 import 'src/widgets.dart';
+//https://pub.dev/packages/table_calendar <-- where I got the table calendar
 
-class CalendarPage extends StatelessWidget {
+class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
 
+  @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+  CalendarFormat calendarFormat = CalendarFormat.month;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,10 +23,25 @@ class CalendarPage extends StatelessWidget {
         title: const Text('BusyBees'),
       ),
       body: TableCalendar(
+        headerStyle: const HeaderStyle(
+        formatButtonVisible: false,
+        ),
         firstDay: DateTime.utc(2010, 10, 16),
         lastDay: DateTime.utc(2030, 3, 14),
         focusedDay: DateTime.now(),
+      selectedDayPredicate: (day) {
+  return isSameDay(_selectedDay, day);
+},
+onDaySelected: (selectedDay, focusedDay) {
+  setState(() {
+    _selectedDay = selectedDay;
+    _focusedDay = focusedDay;
+  });
+},
+onPageChanged: (focusedDay) {
+  focusedDay = focusedDay;
+},
     )
     );
   }
-}
+  }
