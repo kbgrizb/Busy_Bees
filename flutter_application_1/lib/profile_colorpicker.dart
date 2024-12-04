59fac6ce63032e57f1af0d04297aab3cb4b1d30f
Sadie Lewis <LewisSN@hendrix.edu>
2024-12-03 21:41:14 -0600

    Added color picker dialog

---
+++ flutter_application_1/lib/profile_colorpicker.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class ProfileColorPicker extends StatefulWidget {
+  final Color initialColor;
+
+  const ProfileColorPicker({super.key, required this.initialColor});
+
+  @override
+  State<ProfileColorPicker> createState() => _ProfileColorPickerState();
+}
+
+class _ProfileColorPickerState extends State<ProfileColorPicker> {
+  late Color selectedColor;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedColor = widget.initialColor;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Choose a Color"),
+      content: DropdownButton<Color>(
+        value: selectedColor,
+        items: const [
+          DropdownMenuItem(
+            value: Colors.yellow,
+            child: Text("Yellow", style: TextStyle(color: Colors.yellow)),
+          ),
+          DropdownMenuItem(
+            value: Colors.green,
+            child: Text("Green", style: TextStyle(color: Colors.green)),
+          ),
+          DropdownMenuItem(
+            value: Colors.blue,
+            child: Text("Blue", style: TextStyle(color: Colors.blue)),
+          ),
+          DropdownMenuItem(
+            value: Colors.red,
+            child: Text("Red", style: TextStyle(color: Colors.red)),
+          ),
+          DropdownMenuItem(
+            value: Colors.orange,
+            child: Text("Orange", style: TextStyle(color: Colors.orange)),
+          ),
+        ],
+        onChanged: (Color? value) {
+          setState(() {
+            if (value != null) {
+              selectedColor = value;
+            }
+          });
+        },
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(null);
+          },
+          child: const Text("Cancel"),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(selectedColor);
+          },
+          child: const Text("Confirm"),
+        ),
+      ],
+    );
+  }
+}
